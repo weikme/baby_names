d597ea4e89b2dbf895cd9d54e6af1c9193e73e3b
Yaroslav Siomka <orange.lviv.99@gmail.com>
2020-09-07 13:38:48 +0300

    added comments

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -4,6 +4,7 @@ import 'widgets/build_body.dart';
 
 // ignore: use_key_in_widget_constructors
 class MyHomePageState extends StatelessWidget {
+  ///I did it with [extension ... on MyHomePageState] to learn new things (it can also be done with [StatelessWidget]
   @override
   Widget build(BuildContext context) {
     return Scaffold(
